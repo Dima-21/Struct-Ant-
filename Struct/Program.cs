b41ef8a7e01653fb935ff9b676048842a76aac18
student <student@K10-13>
2018-04-02 17:02:47 +0300

    Add project files.

---
+++ Struct/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    class Program
+    {
+        //struct Test
+        //{
+        //    public int A;
+        //    public Test(int a)
+        //    {
+        //        A = a;
+        //    }
+        //}
+
+        //static void Func(Test ts)
+        //{
+        //    ts.A = 100500;
+        //}
+        static void Main(string[] args)
+        {
+            //int a = 8;
+            //Test t;
+            //t.A = 10;
+            //Func(t);
+            //Console.WriteLine(t.A);
+           
+        }
+    }
+}
---
+++ Unit/Unit.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Unit
+{
+    class Unit
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public int Rotation { get; set; }
+        public double Power { get; set; }
+        public void TurnRight()
+        {
+           Rotation -= 5;
+        }
+        public void TurnLeft()
+        {
+            Rotation += 5;
+        }
+
+        public void Move()
+        {
+            double r1 = Rotation * 180 / Math.PI;
+            X = Math.Sin(r1) * Power + X;
+            Y = Math.Cos(r1) * Power + Y;
+        }
+
+        public void Accelerator()
+        {
+            Power += 0.7;
+        }
+
+
+        public void Stop()
+        {
+            Power -= 0.9;
+        }
+        public void TurnToPoint(double x, double y)
+        {
+            double a = x - X;
+            double b = y - Y;
+            double c = Math.Sqrt(a * a + b * b);
+            double sinac = Math.Atan(a / b) * 180 / Math.PI;
+
+        }
+        public override string ToString()
+        {
+            return $"X: {X}, Y: {Y};";
+        }
+
+    }
+}
